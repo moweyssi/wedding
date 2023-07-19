34af0d2f76fc6a98b7d5f91884e3d0d284e839a5
Maxim <maxim.oweyssi@est.org.uk>
2023-07-19 10:53:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,43 @@
 import streamlit as st
 
 st.image("info.png")
+
+##Dearest Friends,
+#Allow us to share with you some more details of the highly-anticipated wedding of the millennium. We hope the following points will give you an idea of the festivities on that glorious day:
+#Who: Anna and Max
+#When: Saturday, September 2, 2023
+#Where: The beautiful Chříč Castle at Chříč 1
+#Ceremony: Commencing at 2:00 PM Arrivals: From 12:30 PM to 1:30 PM. Light refreshments will be
+#provided. For those camping enthusiasts, we suggest aiming for the start of the arrival window.
 st.image("spacer.png")
+
+##festivities
+#Dear friends, on the same day, another wedding will take place in the village. Therefore, please follow the navigation marked with initials A + M. Additionally, the traditional Chříč
+#Rally will also be happening in the village. Yes, you heard it right. We perceive it as a positive and sincere
+#start to our married life, one filled with surprises and challenges. In case the traditional automotive
+#festivities hinder any of the many roads to Chříč, we will keep you informed.
+st.image("spacer.png")
+
+##and now. the grand evening approaches!
+#The party will commence with a toast at approximately 3:30 PM, launching a radiant ball on the castle
+#terrace. To harmonise with the atmosphere, we urge you to bring something that sparkles. It can be
+#jewellery, a piece of clothing, flat shoes, or anything else. Glitter has no limits. We kindly ask all guests to avoid clumsy footwear that could lead to unwanted dance mishaps. Instead, we recommend stylish and comfortable shoes so you can enjoy the festivities carefree.
+st.image("spacer.png")
+
+##gifts
+#Due to our scattered possessions across various European lands and beyond, we do not wish for traditional
+#material gifts. However, if you would be so kind and wish to support us, we would gratefully accept a
+#financial contribution towards our journey into the world, which will take place in the winter months. Your
+#assistance will help us explore exotic destinations and build shared memories that will fuel our peaceful
+#marriage during times of conflict and challenge.
+st.image("spacer.png")
+
+##accomodation options:
+#Camping: Space will be allocated for setting up tents on-site. Local school: If you prefer the comfort of the school, please bring a sleeping mat and a sleeping bag. Riverside camps: Just 8 minutes away by car, there are two camps with cabins that have beds and bedding. Each cabin accommodates 4 people. We have approximately 50 spots reserved, so please let us know, and
+#we will secure a spot for you. Hotel: For those who prefer the comfort of a hotel, we will gladly provide you with recommendations. However, the costs of the hotel will unfortunately be on your own
+#Transportation to the hotel and campsites will be arranged. Dearest friends, your presence is the most precious gift to us on this legendary day. With warmth, love, and excitement,
+#
+Anna & Max
+
 
 st.image("footer.png")
