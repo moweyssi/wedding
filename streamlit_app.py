3d03ad66c69ee4fc6b9481c76dbeb1d5a36099b7
Maxim <maxim.oweyssi@est.org.uk>
2023-07-19 11:41:43 +0100

    r

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,37 +7,6 @@ hide_decoration_bar_style = '''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 
-t = st.radio("Toggle to see font change", [True, False])
-
-if t:
-    st.markdown(
-        """
-        <style>
-@font-face {
-  font-family: 'Tangerine';
-  font-style: normal;
-  font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
-
-    html, body, [class*="css"]  {
-    font-family: 'Tangerine';
-    font-size: 48px;
-    }
-    </style>
-
-    """,
-        unsafe_allow_html=True,
-    )
-
-"# Hello"
-
-"""This font will look different, based on your choice of radio button"""
-
-
-
-
 st.image("info.png")
 
 st.subheader("Dearest Friends,")
