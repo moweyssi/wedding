d850dbb85ea8c1c4a818fe2505922709af324208
Maxim <maxim.oweyssi@est.org.uk>
2023-07-19 11:57:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,18 +48,6 @@ With warmth, love, and excitement,
 st.image("signature.png")
 
 
-
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.image("footer.png")
-with col2:
-    st.button("balonky", on_click=st.balloons)
-with col3:
-    st.button("snih", on_click=st.snow)
-
-
-
 st.markdown(
     """
     <iframe src="https://docs.google.com/document/d/e/2PACX-1vQFnGOog3dOb0TflEM9Md8sDOPvdY3OOFssiRbZS7_nhQ0P2ZPs6WgDYmkaSBoY9nxMRCMGjjDXlGJQ/pub?embedded=true"></iframe>
@@ -67,3 +55,14 @@ st.markdown(
     unsafe_allow_html=True
 
 )
+
+st.image("footer.png")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.button("balonky", on_click=st.balloons)
+with col2:
+    st.button("snih", on_click=st.snow)
+
+
+
