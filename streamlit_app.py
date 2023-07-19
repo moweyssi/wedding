55464ab8ee83726ebdae1e2fe84506511adfdab9
Maxim <maxim.oweyssi@est.org.uk>
2023-07-19 11:55:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,17 @@ with col2:
 with col3:
     st.button("snih", on_click=st.snow)
 
-st.markdown(
-    """src="https://docs.google.com/forms/d/e/1FAIpQLSeNEAVH0J3Fob8uPX2qJz3puVX5HhE_LTs2CqG5mkcmMSsrOg/viewform" name="rsvp" height="300px" width="100%" title="RSVP form"></iframe>""",
-    unsafe_allow_html=True
+st.components.html('''
+  <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeNEAVH0J3Fob8uPX2qJz3puVX5HhE_LTs2CqG5mkcmMSsrOg/viewform"
+      width="1000"
+      height="3000"
+      frameborder="0">
+  </iframe>
+ ''',height=3000,width=1000)
 
-)
+#st.markdown(
+#    """src="https://docs.google.com/forms/d/e/1FAIpQLSeNEAVH0J3Fob8uPX2qJz3puVX5HhE_LTs2CqG5mkcmMSsrOg/viewform" name="rsvp" height="300px" width="100%" title="RSVP form"></iframe>""",
+#    unsafe_allow_html=True
+#
+#)
