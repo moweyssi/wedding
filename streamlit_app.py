f40bc3662af3f9ade1de854397ce63f902252312
Maxim <maxim.oweyssi@est.org.uk>
2023-08-21 23:32:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,14 +82,14 @@ if language == "ENG":
         """
     with st.expander("Harmonogram"):
         """
-         - 14:00 Ceremony 
-         - 14:30 Gratulations for those who are not staying overnight
-         - 15:00 First dance 
-         - 15:30 Party 
-         - 17:00 Anna and Max have a coffee with family
+         . 14:00 Ceremony 
+         . 14:30 Gratulations for those who are not staying overnight
+         . 15:00 First dance 
+         . 15:30 Party 
+         . 17:00 Anna and Max have a coffee with family
 
         During the party, there will be speeches, throwing of the bouquet, etc. 
-        
+
         It's possible, even likely, that parts of the program will be delayed, as is common at weddings.
         """
     with st.expander("What to Wear"):
